cb4dcfeb5d98f5d0ac43cb8526679fcd6bb95cad
Bo Zhou <bzhou2@ualberta.ca>
2016-11-29 20:19:11 -0700

    rnn model done

--- rnn2.py
+++ rnn2.py
@@ -8,11 +8,11 @@ mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 lr = 0.001
 training_iters = 100000
 batch_size = 128
-display_step = 10
+#display_step = 10
 
 n_inputs = 28
 n_steps = 28
-n_hidden_unis = 128
+n_hidden_units = 128
 n_classes = 10
 
 #tf Graph input
@@ -22,15 +22,15 @@ y = tf.placeholder(tf.float32, [None, n_classes])
 #Define weights
 weights = {
         # (28, 128)
-        'in' : tf.Variable(tf.random_normal([n_inputs, n_hidden_unis])),
+        'in' : tf.Variable(tf.random_normal([n_inputs, n_hidden_units])),
         # (128, 10)
-        'out' : tf.Variable(tf.random_normal([n_hidden_unis, n_classes]))
+        'out' : tf.Variable(tf.random_normal([n_hidden_units, n_classes]))
         }
 biases = {
         # (128,)
-        'in' : tf.Variable(tf.constant(0.1, shape=[n_hidden_unis, ])),
+        'in' : tf.Variable(tf.constant(0.1, shape=[n_hidden_units, ])),
         # (10,)
-        'out' : tf.Variable(tf.constant(0.1, shape=[n_class,]))
+        'out' : tf.Variable(tf.constant(0.1, shape=[n_classes, ]))
         }
 
 def RNN(X, weight, biases):
@@ -41,14 +41,14 @@ def RNN(X, weight, biases):
     # X_in ==> (128 batch * 28 steps, 128 hidden)
     X_in = tf.matmul(X, weights['in']) + biases['in']
     # X_in ==> (128 batch, 28 steps, 128 hidden)
-    X_in = tf.reshape(X_in, [-1, n_steps, n_hidden_unis])
+    X_in = tf.reshape(X_in, [-1, n_steps, n_hidden_units])
 
 
     # cell
     lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden_units, forget_bias=1.0, state_is_tuple=True)
     # lstm cell is divided into two parts (c_state, m_state)
     _init_state = lstm_cell.zero_state(batch_size, dtype=tf.float32)
-    output, states = tf.nn.dynamic_rnn(lstm_cell, X_in, initial_state=_init_state, time_major=False)
+    outputs, states = tf.nn.dynamic_rnn(lstm_cell, X_in, initial_state=_init_state, time_major=False)
 
 
 
@@ -58,10 +58,10 @@ def RNN(X, weight, biases):
     # or
     # unpack to list [(batch, outputs)..] * steps
     outputs = tf.unpack(tf.transpose(outputs, [1, 0, 2]))
-    result = tf.mat
+    results = tf.matmul(outputs[-1], weights['out']) + biases['out']
 
 
-    results = None
+    #results = None
     return results
 
 
@@ -69,10 +69,11 @@ pred = RNN(x, weights, biases)
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
 train_op = tf.train.AdamOptimizer(lr).minimize(cost)
 
-correct_pred = tf.equal(tf.argmax(pred, 1), tf.argman(y, 1))
+correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
-init = tf.initialize_all_variables()
+#init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
 with tf.Session() as sess:
     sess.run(init)
     step = 0
@@ -84,7 +85,7 @@ with tf.Session() as sess:
             y: batch_ys
             })
         if step % 20 == 0:
-            print(sess,run(accuracy, feed_dict = {
+            print(sess.run(accuracy, feed_dict = {
                 x: batch_xs,
                 y: batch_ys,
                 }))
